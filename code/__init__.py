ede19922e60879638ddfad46854c7f6a251eb94b
Tzeng Yuxio <tzengyuxio@gmail.com>
2012-12-04 18:57:56 +0800

    初步修正過的 python 指令碼 1. 移除不必要代碼 2. 補上漏掉的檔案 3. 整理邏輯

--- code/__init__.py
+++ code/__init__.py
@@ -3,11 +3,12 @@
 import os, sys
 import shutil
 
-dirOpencc = "D:\\Apps\\opencc\\"
-# dirOrigin = "D:\\Steam\\steamapps\\common\\sid meier's civilization v\\assets\\"
-dirOrigin = "D:\\civ5zh\\civ5mock\\Assets\\"
-#dirOutput = "D:\\work\\civ5zh\\Sources\\assets\\"
-dirOutput = "D:\\civ5zh\\civ5mock\\AssetsOutput\\"
+dirOpencc = "..\\bin\\opencc\\"
+dirOrigin = "..\\civclub\\civ5root\\"
+dirOutput = ".\\output\\"
+dirAssets = "Assets\\"
+
+absDirOpencc = os.path.abspath(dirOpencc)
 
 specialFiles = ['Gameplay\\XML\\NewText\\CIV5Credits_zh_CN.txt',
                 'Gameplay\\XML\\NewText\\CIV5Credits_zh_TW.txt',
@@ -19,61 +20,7 @@ specialFiles = ['Gameplay\\XML\\NewText\\CIV5Credits_zh_CN.txt',
 countDir = 0
 countFile = 0
 
-def doEntry(entry, isInZhCN):
-    if os.path.isdir(entry):
-        doDir(entry, isInZhCN)
-    elif isInZhCN:
-        doFile(entry)
-        
-def doDir(dir, isInZhCN):
-    lastDir = os.getcwd()
-    os.chdir(dir)
-    
-    isZhCN = os.path.basename(dir) == 'zh_CN'
-    # 如果是位於簡中語系資料夾下，就先產生對應的繁中語系資料夾
-    if isInZhCN or isZhCN:
-        global countDir
-        countDir += 1
-        
-        dirZhTW = dir.replace('zh_CN', 'zh_TW')
-        if os.path.isdir(dirZhTW):
-            print '[dir] %s already exists' % (dirZhTW,)
-        else:
-            print '[dir] make dir: %s'
-            os.makedirs(dirZhTW)
-            
-    entryList = os.listdir(dir)
-    for entry in entryList:
-        absPath = os.path.abspath(entry)
-        doEntry(absPath, isInZhCN or isZhCN)
-        
-    os.chdir(lastDir)
-    
-def doFile(txtFile):
-    if txtFile[-3:] != 'xml':
-        return
-    
-    global countFile
-    countFile += 1
-    txtFileZhTW = txtFile.replace('zh_CN', 'zh_TW')
-    txtFileZhTWOrig = txtFileZhTW + '.orig'
-    print '[file] create zh_TW copy of %s' % (txtFile,)
-    os.chdir(dirOpencc)
-    # 轉碼
-    cmdOpencc = 'opencc.exe -i "%s" -o "%s" -c zhs2zht.ini' % (txtFile, txtFileZhTWOrig)
-    os.system(cmdOpencc)
-    # 替換檔案內文字
-    input = open(txtFileZhTWOrig)
-    output = open(txtFileZhTW, 'w')
-    for s in input:
-        output.write(s.replace("zh_CN", "zh_TW"))
-    input.close()
-    output.close()
-    # 刪除暫存檔
-    os.remove(txtFileZhTWOrig)
-    # 回到原資料夾
-    os.chdir(os.path.dirname(txtFile))
-    
+
 def copyLocaleFiles(path):
     if not os.path.isdir(path):
         return
@@ -85,6 +32,7 @@ def copyLocaleFiles(path):
     if dirName == 'zh_CN' or dirName == 'zh_TW':
         dstPath = path.replace(dirOrigin, dirOutput)
         print "[dir] coping dir: %s to %s" % (path, dstPath)
+        countDir++
         shutil.copytree(path, dstPath)
     else:
         subPathList = os.listdir(path)
@@ -92,6 +40,7 @@ def copyLocaleFiles(path):
             copyLocaleFiles(os.path.abspath(subPath))
             
     os.chdir(lastDir)
+
 
 def processPath(path, inZhCn):
     lastPath = os.getcwd()
@@ -106,31 +55,28 @@ def processPath(path, inZhCn):
     
     if isZhCn or inZhCn:
         print os.path.basename(path) + "\t" + os.path.abspath(path)
-    
-    fileList = os.listdir(path)
+
+    fileList = os.listdir(".")
     for subPath in fileList:
         absPath = os.path.abspath(subPath)
         if os.path.isdir(absPath):
             processPath(absPath, isZhCn or inZhCn)
-        elif (isZhCn or inZhCn) and subPath[-3:] == "xml":
+        elif (isZhCn or inZhCn) and (subPath[-3:] == "xml" or subPath[-3:] == "XML"):
             absPathTw = absPath.replace("zh_CN", "zh_TW")
-            #shutil.copyfile(absPath, absPathTw)
-            os.chdir("D:\\Apps\\opencc\\")
-            # 轉碼
-            cccmd = "opencc.exe -i \"" + absPath + "\" -o \"" + absPathTw + ".orig\" -c zhs2zht.ini"
-            os.system(cccmd)
-            # 替換 zh_CN 為 zh_TW
+            convertZ(absPath, absPathTw + '.orig')
+            
+            ## 將檔案內文字 zh_CN 替換為 zh_TW
             input = open(absPathTw+".orig")
             output = open(absPathTw, 'w')
             for s in input:
                 output.write(s.replace("zh_CN", "zh_TW"))
             input.close()
             output.close()
+            ## 刪除暫存檔
             os.remove(absPathTw+".orig")
-            os.chdir(path)
             
-  
     os.chdir(lastPath)
+
 
 def setupLocaleFiles():
     # 刪除目的資料夾
@@ -151,22 +97,46 @@ def setupLocaleFiles():
         shutil.copy(origFile, targetFile)
 
 
-def preprocess(path):
-    xmlFile = dirOrigin + 'Gameplay\\XML\\NewText\\Chinese.xml'
-    xmlCn = dirOrigin + 'Gameplay\\XML\\NewText\\ChineseSimp.xml'
-    xmlTw = dirOrigin + 'Gameplay\\XML\\NewText\\ChineseTrad.xml'
-    creditsCn = dirOrigin + 'Gameplay\\XML\\NewText\\CIV5Credits_zh_CN.txt'
-    creditsTw = dirOrigin + 'Gameplay\\XML\\NewText\\CIV5Credits_zh_TW.txt'
-    os.rename(xmlFile, xmlCn)
-    shutil.copy(xmlCn, xmlTw)
-    shutil.copy(creditsCn, creditsTw)
-    # 需手動修改內容
+def preprocess():
+    ## 刪除目的資料夾
+    cmdRmdir = 'rmdir /s /q "%s"' % dirOutput
+    os.system(cmdRmdir)
+    ## 重建目的資料夾
+    os.makedirs(dirOutput)
+    
+    shutil.copytree(dirOrigin+dirAssets, dirOutput+dirAssets)
 
-os.chdir(dirOrigin)
-preprocess(dirOrigin)
-processPath(dirOrigin, False)
-#doEntry(dirOrigin, False)
-setupLocaleFiles()
+    ## 需手動修改內容
+    prefix = os.path.abspath(dirOutput + dirAssets)
+    xmlFile = prefix + '\\Gameplay\\XML\\NewText\\Chinese.xml'
+    xmlCn = prefix + '\\Gameplay\\XML\\NewText\\ChineseSimp.xml'
+    xmlTw = prefix + '\\Gameplay\\XML\\NewText\\ChineseTrad.xml'
+    creditsCn = prefix + '\\Gameplay\\XML\\NewText\\CIV5Credits_zh_CN.txt'
+    creditsTw = prefix + '\\Gameplay\\XML\\NewText\\CIV5Credits_zh_TW.txt'
+    dlcCreditsCn = prefix + '\\DLC\\Expansion\\Gameplay\\XML\\Text\\CIV5Credits_zh_CN.txt'
+    dlcCreditsTw = prefix + '\\DLC\\Expansion\\Gameplay\\XML\\Text\\CIV5Credits_zh_TW.txt'
+    os.rename(xmlFile, xmlCn)
+    convertZ(xmlCn, xmlTw) # 這個檔不能單單轉換
+    convertZ(creditsCn, creditsTw)
+    convertZ(dlcCreditsCn, dlcCreditsTw)
+
+
+## src, dst 的路徑需為絕對路徑
+def convertZ(src, dst):
+    lastPath = os.getcwd()
+    os.chdir(absDirOpencc)
+
+    ## 轉碼
+    cccmd = 'opencc.exe -i "%s" -o "%s" -c zhs2zht.ini' % (src, dst)
+    os.system(cccmd)
+    countFile++
+
+    os.chdir(lastPath)
+
+
+preprocess()
+processPath(dirOutput, False)
+# setupLocaleFiles()
 
 print 'count of locale dirs: %d' % (countDir,)
 print 'count of locale files: %d' % (countFile,)
